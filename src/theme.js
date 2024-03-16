5524d4ca617a3912bbbe4c6d38068b05b97eedb7
phuochieu <phieeus99@gmail.com>
2024-03-16 15:41:10 +0700

    Mode Light Dark

--- src/theme.js
+++ src/theme.js
@@ -1,20 +1,19 @@
-import { createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
-
-// Create a theme instance.
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: red[500]
+import { teal, deepOrange, cyan, orange } from '@mui/material/colors'
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
+// Create a theme instance
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: teal,
+        secondary: deepOrange
+      }
     },
-    secondary: {
-      main: '#19857b'
-    },
-    error: {
-      main: red.A400
-    },
-    text: {
-      secondary: red[500]
+    dark: {
+      palette: {
+        primary: cyan,
+        secondary: orange
+      }
     }
   }
 })
