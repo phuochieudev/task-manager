32605f790cc8d01f265799749feb9e98927a63bc
phuochieu <phieeus99@gmail.com>
2024-03-26 15:50:04 +0700

    Code App Bar P3

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -22,7 +22,9 @@ function AppBar() {
       height: (theme) => theme.trello.appBarHeight,
       display: 'flex',
       alignItems: 'center',
-      justifyContent: 'space-between'
+      justifyContent: 'space-between',
+      gap: 2,
+      overflow: 'auto'
     }}>
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
         <AppsIcon sx={{ color: 'primary.main' }}/>
@@ -31,14 +33,17 @@ function AppBar() {
           <Typography variant='span' sx={{ fontSize: '1.4rem', fontWeight:'bold', color: 'primary.main' }}>
           Trello</Typography>
         </Box>
-        <Workspace/>
-        <Recent/>
-        <Starred/>
-        <Templates/>
-        <Button variant="outlined">Create</Button>
+        <Box sx={{ display: { xs: 'none', md: 'flex' }, gap: 1 }}>
+          <Workspace/>
+          <Recent/>
+          <Starred/>
+          <Templates/>
+          <Button variant="outlined">Create</Button>
+        </Box>
+
       </Box>
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
-        <TextField id="outlined-search" label="Search..." type="search" size='small'/>
+        <TextField id="outlined-search" label="Search..." type="search" size='small' sx={{ minWidth: '120px' }}/>
         <ModeSelect/>
         <Tooltip title="Notification"> <Badge color="secondary" variant="dot" sx={{ cursor: 'pointer' }}>
           <NotificationsNoneIcon sx={{ color: 'primary.main' }}/>
